2f5905a875e4a0911f74c0c0cd677f8f989e630b
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-03-27 16:55:32 +0100

    fix linter

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import { Book, Books } from './modules/books.js';
-import { DateTime } from './node_modules/luxon/src/luxon.js'
-import { showForm, showContact, showList } from './modules/navigation.js';
-import { listLink, formLink, contactLink, formSection, contact } from './modules/navigation.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+import {
+  showForm, showContact, showList, listLink, formLink, contactLink, formSection, contact,
+} from './modules/navigation.js';
 
 const form = document.getElementById('form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const time = document.getElementById('time');
 
-let now = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
-
-console.log(now);
-
-// updata the time every second
 setInterval(() => {
   time.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
 }, 1000);
-
 
 formSection.style.display = 'none';
 contact.style.display = 'none';
@@ -24,7 +19,6 @@ contact.style.display = 'none';
 formLink.addEventListener('click', () => showForm());
 contactLink.addEventListener('click', () => showContact());
 listLink.addEventListener('click', () => showList());
-
 
 const books = new Books();
 
@@ -37,4 +31,3 @@ form.onsubmit = (e) => {
 
 books.getBooks();
 books.renderBooks();
-
--- modules/books.js
+++ modules/books.js
@@ -1,60 +1,59 @@
+/* eslint max-classes-per-file: ["error", 2] */
 const booksList = document.getElementById('books-list');
 
 export class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-      this.id = Math.random();
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Math.random();
   }
-  
-  export class Books {
-    constructor() {
-      this.books = [];
-    }
-  
-    addBook(book) {
-      this.books.push(book);
-      this.saveBooks();
-      this.renderBooks();
-    }
-  
-    removeBook(id) {
-      this.books = this.books.filter((book) => book.id !== id);
-    }
-  
-    saveBooks() {
-      localStorage.setItem('books', JSON.stringify(this.books));
-    }
-  
-    getBooks() {
-      const storedBooks = localStorage.getItem('books');
-      if (storedBooks) {
-        this.books = JSON.parse(storedBooks);
-      }
-      return [];
-    }
-  
-    renderBooks() {
-      booksList.innerHTML = '';
-      this.books.forEach((book) => {
-        const li = document.createElement('li');
-        li.classList.add('book-item');
-        const p = document.createElement('p');
-        p.innerHTML = `${book.title} by ${book.author}`;
-        const button = document.createElement('button');
-        button.classList.add('remove-btn');
-        button.innerHTML = 'Remove';
-        button.onclick = (e) => {
-            this.removeBook(book.id);
-            this.saveBooks();
-            this.renderBooks();
-        }
-        li.appendChild(p);
-        li.appendChild(button);
-        booksList.appendChild(li);
-      });
-    }
+}
+
+export class Books {
+  constructor() {
+    this.books = [];
   }
 
-  
+  addBook(book) {
+    this.books.push(book);
+    this.saveBooks();
+    this.renderBooks();
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+  }
+
+  saveBooks() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  getBooks() {
+    const storedBooks = localStorage.getItem('books');
+    if (storedBooks) {
+      this.books = JSON.parse(storedBooks);
+    }
+    return [];
+  }
+
+  renderBooks() {
+    booksList.innerHTML = '';
+    this.books.forEach((book) => {
+      const li = document.createElement('li');
+      li.classList.add('book-item');
+      const p = document.createElement('p');
+      p.innerHTML = `${book.title} by ${book.author}`;
+      const button = document.createElement('button');
+      button.classList.add('remove-btn');
+      button.innerHTML = 'Remove';
+      button.onclick = () => {
+        this.removeBook(book.id);
+        this.saveBooks();
+        this.renderBooks();
+      };
+      li.appendChild(p);
+      li.appendChild(button);
+      booksList.appendChild(li);
+    });
+  }
+}
